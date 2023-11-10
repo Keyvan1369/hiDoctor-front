eb71ca8ee5a52218dc04443f239bdaf605eeac4f
Keyvan <keyvanhojabr@gmail.com>
2023-11-10 12:16:02 +0330

    setup routing and redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Home from "./pages/home/home.jsx";
 import Login from "./pages/login/login.jsx";
 import Register from "./pages/register/register.jsx";
+import {useSelector} from "react-redux";
 
 function App() {
 
-    return (
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-        </Routes>
-    )
+    const {isAuthenticated} = useSelector(store => store.auth)
+
+    return (<Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={!isAuthenticated ? <Login/> : <Navigate to="/"/>}/>
+        <Route path="/register" element={isAuthenticated ? <Register/> : <Navigate to="/"/>}/>
+        <Route path="*" element={<Navigate to="/"/>}/>
+    </Routes>)
 }
 
 export default App
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import style from "./header.module.scss";
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 const Header = () => {
+
+    const isAuthenticated = useSelector(store => store.auth.isAuthenticated)
     return (
         <header className={style.header}>
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                    <li><Link to="/login">Login</Link></li>
+                    {
+                        isAuthenticated ? <>
+                            <li><Link to="/register">Panel</Link></li>
+                        </> : <>
+                            <li><Link to="/register">Register</Link></li>
+                            <li><Link to="/login">Login</Link></li>
+                        </>
+                    }
                 </ul>
             </nav>
         </header>
